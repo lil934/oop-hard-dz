0f54ae720e0292c59468ea7a66a1cca9b5d4f2e6
lil934 <l.i.09@list.ru>
2025-11-04 16:55:39 +0500

    Исакова1

--- src/InvalidUserParamsException.java
+++
@@ -1,5 +0,0 @@
-public class InvalidUserParamsException extends RuntimeException {
-    public InvalidUserParamsException(long id, String message) {
-        super("Ошибка пользователя с ID " + id + ": " + message);
-    }
-}
